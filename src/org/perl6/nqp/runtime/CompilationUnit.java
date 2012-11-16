fa2df2d8b0db0f7bddd39a095ddd1c56532ec79f
jnthn <jnthn@jnthn.net>
2012-11-16 23:41:26 +0100

    Very preliminary entry point/setup code.

--- src/org/perl6/nqp/runtime/CompilationUnit.java
+++ src/org/perl6/nqp/runtime/CompilationUnit.java
@@ -7,6 +7,27 @@ package org.perl6.nqp.runtime;
  */
 public abstract class CompilationUnit {
 	/**
+	 * When a compilation unit is serving as the main entry point, its main
+	 * method will just delegate to here. Thus this needs to trigger some
+	 * initialization work and then invoke the required main code.
+	 */
+	public static void enterFromMain(Class<?> cuType, int entryCodeRefIdx, String[] argv)
+			throws InstantiationException, IllegalAccessException {
+		CompilationUnit cu = setupCompilationUnit(cuType);
+		ThreadContext tc = new ThreadContext();
+		cu.InvokeCode(tc, entryCodeRefIdx);
+	}
+	
+	/**
+	 * Takes the class object for some compilation unit and sets it up. 
+	 */
+	public static CompilationUnit setupCompilationUnit(Class<?> cuType)
+			throws InstantiationException, IllegalAccessException {
+		CompilationUnit cu = (CompilationUnit)cuType.newInstance();
+		return cu;
+	}
+
+	/**
 	 * The JVM doesn't have first-class delegate types. Using the anonymous
 	 * class pattern to fake that won't fly too well as we'll end up with many
 	 * thousands of them. Instead, a code reference identifies a compilation
