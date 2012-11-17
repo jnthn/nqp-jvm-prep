612d1b9b12a340ee4b08d7e008cca50ba7b849f3
jnthn <jnthn@jnthn.net>
2012-11-17 14:28:00 +0100

    Our first Ops entries: various forms of say.

--- src/org/perl6/nqp/runtime/Ops.java
+++ src/org/perl6/nqp/runtime/Ops.java
@@ -5,5 +5,18 @@ package org.perl6.nqp.runtime;
  * JVM makes available.
  */
 public final class Ops {
+	public static long say(long v) {
+		System.out.println(v);
+		return v;
+	}
 	
+	public static double say(double v) {
+		System.out.println(v);
+		return v;
+	}
+	
+	public static String say(String v) {
+		System.out.println(v);
+		return v;
+	}
 }
