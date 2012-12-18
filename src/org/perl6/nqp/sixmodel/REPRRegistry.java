9e0fae9d3b677ed39150ae838ef3b0b3b90abf18
jnthn <jnthn@jnthn.net>
2012-12-19 00:32:25 +0100

    Add REPR registry.

---
+++ src/org/perl6/nqp/sixmodel/REPRRegistry.java
@@ -0,0 +1,33 @@
+package org.perl6.nqp.sixmodel;
+
+import org.perl6.nqp.sixmodel.reprs.*;
+import java.util.*;
+
+public class REPRRegistry {
+	private static HashMap<String, Integer> reprIdMap = new HashMap<String, Integer>();
+	private static ArrayList<REPR> reprs = new ArrayList<REPR>();
+	
+	public static REPR getByName(String name) {
+		Integer idx = reprIdMap.get(name);
+		if (idx == null)
+			throw new RuntimeException("No REPR " + name);
+		return getById(idx);
+	}
+	
+	public static REPR getById(int id) {
+		if (id < reprs.size())
+			return reprs.get(id);
+		else
+			throw new RuntimeException("No REPR " + new Integer(id).toString());
+	}
+	
+	private static void addREPR(String name, REPR REPR) {
+		reprIdMap.put(name, reprs.size());
+		reprs.add(REPR);
+	}
+	
+	public static void setup() {
+		addREPR("KnowHOWREPR", new KnowHOWREPR());
+		addREPR("KnowHOWAttribute", new KnowHOWAttribute());
+	}
+}
