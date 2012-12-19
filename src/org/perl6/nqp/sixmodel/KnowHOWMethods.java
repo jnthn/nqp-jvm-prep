3078072d0b9f06676b6559adb31929029ab3de32
jnthn <jnthn@jnthn.net>
2012-12-19 12:13:16 +0100

    Port type creation.

--- src/org/perl6/nqp/sixmodel/KnowHOWMethods.java
+++ src/org/perl6/nqp/sixmodel/KnowHOWMethods.java
@@ -10,7 +10,35 @@ import org.perl6.nqp.sixmodel.reprs.*;
  */
 public class KnowHOWMethods extends CompilationUnit {
 	public void new_type(ThreadContext tc) {
-		// TODO
+	    /* Get arguments. */
+	    SixModelObject self = Ops.posparam_o(tc.curFrame, 0);
+	    String repr_arg = null; // TODO: Needs named args/params
+	    String name_arg = null; // TODO: Needs named args/params
+	    if (self == null || !(self instanceof KnowHOWREPRInstance))
+			throw new RuntimeException("KnowHOW methods must be called on object instance with REPR KnowHOWREPR");
+	    
+	    /* We first create a new HOW instance. */
+	    SixModelObject HOW = self.st.REPR.Allocate(tc, self.st);
+	    
+	    /* See if we have a representation name; if not default to P6opaque. */
+	    String repr_name = repr_arg != null ? repr_arg : "P6opaque";
+	        
+	    /* Create a new type object of the desired REPR. (Note that we can't
+	     * default to KnowHOWREPR here, since it doesn't know how to actually
+	     * store attributes, it's just for bootstrapping knowhow's. */
+	    REPR repr_to_use = REPRRegistry.getByName(repr_name);
+	    SixModelObject type_object = repr_to_use.TypeObjectFor(tc, HOW);
+	    
+	    /* See if we were given a name; put it into the meta-object if so. */
+	    HOW.initialize(tc, HOW.st);
+	    if (name_arg != null)
+	    	((KnowHOWREPRInstance)HOW).name = name_arg;
+	    
+	    /* Set .WHO to an empty hash. */
+	    // XXX TODO
+
+	    /* Return the type object. */
+	    Ops.return_o(type_object, tc.curFrame);
 	}
 	
 	public void add_method(ThreadContext tc) {
