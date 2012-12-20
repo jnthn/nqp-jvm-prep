1b12c6396def9815ae6fee6c1c16744ed99320cf
jnthn <jnthn@jnthn.net>
2012-12-20 17:47:40 +0100

    New file missed in previous commit.

---
+++ src/org/perl6/nqp/sixmodel/reprs/P6OpaqueREPRData.java
@@ -0,0 +1,9 @@
+package org.perl6.nqp.sixmodel.reprs;
+
+public class P6OpaqueREPRData {
+	/**
+	 * The JVM class that will be used to represent state storage for this
+	 * type. 
+	 */
+	public Class<?> jvmClass; 
+}
