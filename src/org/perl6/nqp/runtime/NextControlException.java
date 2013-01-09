a25dd96f5211e70dddfce7b104ead66205a26a70
jnthn <jnthn@jnthn.net>
2013-01-09 14:47:15 +0100

    Stub in some control exceptions.
    We'll just use the JVM exceptions for these at the moment.

---
+++ src/org/perl6/nqp/runtime/NextControlException.java
@@ -0,0 +1,5 @@
+package org.perl6.nqp.runtime;
+
+public class NextControlException extends BaseControlException {
+    private static final long serialVersionUID = 2785267898753141738L;
+}
---
+++ src/org/perl6/nqp/runtime/RedoControlException.java
@@ -0,0 +1,5 @@
+package org.perl6.nqp.runtime;
+
+public class RedoControlException extends BaseControlException {
+    private static final long serialVersionUID = 8349113082954301129L;
+}
