f3912956f7fd1deec40daffb047b1a61c1d19d55
jnthn <jnthn@jnthn.net>
2013-01-27 17:10:16 +0100

    Don't duplicately load modules.

--- src/org/perl6/nqp/runtime/LibraryLoader.java
+++ src/org/perl6/nqp/runtime/LibraryLoader.java
@@ -3,10 +3,18 @@ package org.perl6.nqp.runtime;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashSet;
 
 public class LibraryLoader {
+	public static HashSet<String> loaded = new HashSet<String>();
+	
 	public void load(ThreadContext tc, String filename) {		
+		// Don't load the same thing multiple times.
+		if (loaded.contains(filename))
+			return;
+		
 		try {
+			System.err.println("Loading " + filename);
 			// Read in class data.
 			File file = new File(filename);
 			byte[] bytes = new byte[(int)file.length()];
@@ -19,6 +27,10 @@ public class LibraryLoader {
 			CompilationUnit cu = (CompilationUnit)c.newInstance();
 			cu.initializeCompilationUnit(tc);
 			cu.runLoadIfAvailable(tc);
+			System.err.println("Done " + filename);
+			
+			// Note that we already loaded it.
+			loaded.add(filename);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
