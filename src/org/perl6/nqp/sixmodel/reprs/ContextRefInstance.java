d3122400eeb9ec5bca5fb49e9c102a5bbc4cb8b6
jnthn <jnthn@jnthn.net>
2013-01-27 15:59:53 +0100

    Implement object lexical lookup through context.
    Need to do a bunch of other cases, but this is what ModuleLoader needs.

--- src/org/perl6/nqp/sixmodel/reprs/ContextRefInstance.java
+++ src/org/perl6/nqp/sixmodel/reprs/ContextRefInstance.java
@@ -1,8 +1,14 @@
 package org.perl6.nqp.sixmodel.reprs;
 
 import org.perl6.nqp.runtime.CallFrame;
+import org.perl6.nqp.runtime.ThreadContext;
 import org.perl6.nqp.sixmodel.SixModelObject;
 
 public class ContextRefInstance extends SixModelObject {
 	public CallFrame context;
+	
+	public SixModelObject at_key_boxed(ThreadContext tc, String key) {
+        Integer idx = context.codeRef.staticInfo.nTryGetLexicalIdx(key);
+        return idx == null ? null : context.oLex[idx];
+    }
 }
