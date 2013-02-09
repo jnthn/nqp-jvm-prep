bc0b78cbae6397812cc4de5c7dc5da0afa0b67bb
jnthn <jnthn@jnthn.net>
2013-02-09 23:09:26 +0100

    Bump serialization version recognized.

--- src/org/perl6/nqp/sixmodel/SerializationReader.java
+++ src/org/perl6/nqp/sixmodel/SerializationReader.java
@@ -11,7 +11,7 @@ import org.perl6.nqp.sixmodel.reprs.VMHashInstance;
 
 public class SerializationReader {
 	/* The current version of the serialization format. */
-	private final int CURRENT_VERSION = 2;
+	private final int CURRENT_VERSION = 3;
 	
 	/* Various sizes (in bytes). */
 	private final int HEADER_SIZE               = 4 * 16;
